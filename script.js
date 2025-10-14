15a046f2231efe7da612d4fc5ce06c42147e7150
dariabodiakova <dashabodiakova0308@gmail.com>
2025-10-13 18:59:07 -0700

    change style

--- script.js
+++ script.js
@@ -1,16 +1,2 @@
 // Подсветка кода в проектах (Prism.js)
 Prism.highlightAll();
-
-// Smooth scroll для ссылок (если добавятся)
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    anchor.addEventListener('click', function (e) {
-        e.preventDefault();
-        const target = document.querySelector(this.getAttribute('href'));
-        if (target) {
-            target.scrollIntoView({
-                behavior: 'smooth',
-                block: 'start'
-            });
-        }
-    });
-});
